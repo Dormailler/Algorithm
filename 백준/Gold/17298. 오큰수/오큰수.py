e191a985c8e9a4b0ea6db13cd3e585153f32c9c6
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-01-18 07:44:50 +0900

    [Gold IV] Title: 오큰수, Time: 1080 ms, Memory: 150484 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,13 +1,10 @@
-N = int(input())
-arr = list(map(int, input().split()))
-
-ans = [-1] * N  # 초기 정답을 -1로 설정
-stack = [0]  # 첫 인덱스 0을 넣어준다.
-for i in range(1, N):
-    # 만약 스택의 마지막 인덱스의 arr값이 해당 비교하고자 하는 값보다 작다면
-    # 그 값보다 클때까지 해당 값을 오른쪽의 큰 수로 설정해준다.
-    while stack and arr[stack[-1]] < arr[i]:
-        ans[stack.pop()] = arr[i]
-    stack.append(i)  # 해당 인덱스를 무조건 스택에 넣어준다.
-
-print(*ans)
+import sys
+n = int(sys.stdin.readline().rstrip())
+a = list(map(int,sys.stdin.readline().rstrip().split()))
+out = [-1] * n
+stack = [0]
+for i in range(1,n):
+    while stack and a[stack[-1]] < a[i]:
+        out[stack.pop()] = a[i]
+    stack.append(i)
+print(*out)
