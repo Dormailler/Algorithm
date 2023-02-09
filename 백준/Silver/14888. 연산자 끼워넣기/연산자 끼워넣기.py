26e0f7238eef66c1f003483a5f0728e010b1e44e
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-02-10 05:35:49 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 628 ms, Memory: 41964 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,33 +1,18 @@
-from itertools import permutations
-from collections import deque
+import itertools 
+from functools import reduce
+import sys
+import time
 n = int(input())
 li = list(map(int,input().split()))
 cal = list(map(int,input().split()))
-a = ['+','-','*','/']
-st = []
-for i in range(4):
-    for j in range(cal[i]):
-        st.append(a[i])
-ran = list(permutations(st,len(st)))
-q = deque(ran)
-max_result = -1e9
-min_result = 1e9
-for i in range(len(ran)):
-    l = q.popleft()
-    result = li[0]
-    for i in range(1,len(li)):
-        if l[i-1] == '+':
-            result += li[i]
-        elif l[i-1] == '-':
-            result -= li[i]
-        elif l[i-1] == '*':
-            result *= li[i]
-        elif l[i-1] == '/':
-            if result < 0 :
-                result = -(abs(result) // li[i])        
-            else:
-                result //= li[i]
-    max_result = max(max_result, result)
-    min_result = min(min_result, result)
-print(max_result)
-print(min_result)
+def insert_operation(length,input_num,input_oper):
+    ops = {"0" : (lambda x,y :x+y), "1" : (lambda x,y: x-y), "2" : (lambda x,y:x*y),"3":(lambda x,y:x//y if x> 0 else -(abs(x)//y))}
+    oper_permutation = []
+    result = []
+    list(oper_permutation.extend(str(index)*value) for index,value in enumerate(input_oper) if value > 0)
+    permutation = [list(x) for x in set(itertools.permutations(oper_permutation))]
+    for i in permutation:
+        result.append(reduce(lambda x,y : ops[i.pop()](x,y),input_num))
+    print(max(result))
+    print(min(result))
+insert_operation(n,li,cal)
