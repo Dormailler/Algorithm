4612ebccb899835593d03d8f1159d544d171a494
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-03-09 21:44:50 +0900

    [unrated] Title: 덧칠하기, Time: 11.10 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -2,17 +2,14 @@ def solution(n, m, section):
     answer = 0
     stack = []
     num = m
-    t = 0
     for i in section:
         if stack == []:
             stack.append(i)
             continue
-        if (i - stack[-1]+1-t) <= num :
-            num -= (i-stack[-1]+1)
+        if (i - stack[-1]+1) <= num :
+            num -= (i-stack[-1])
             stack.pop()
-            t += 1
         else:
-            t = 0
             num = m
             stack.pop()
             answer += 1
