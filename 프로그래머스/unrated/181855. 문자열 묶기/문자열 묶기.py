86bf42ece9d1b8881672def33694a704a78c745d
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-08-11 17:23:27 +0900

    [unrated] Title: 문자열 묶기, Time: 14.86 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -5,8 +5,4 @@ def solution(strArr):
             dic[len(i)] = 1
         else: 
             dic[len(i)] += 1
-    m = 0
-    for i in dic:
-        if dic[i] > m:
-            m = dic[i]
-    return m
+    return max(dic.values())
