f02409964dd43220a155753846c2da73c60cec9a
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-10-23 00:41:29 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -12,7 +12,7 @@ def solution(maps):
             if maps[i][j] == "S":
                 queue.append([i,j,0])
                 break
-    l = bfs(maps,dx,dy,visited,m,n,queue)
+    l = bfs(maps,dx,dy,visited,m,n,queue,"L")
     if l == -1:
         return -1
     x = l[0]
@@ -20,16 +20,16 @@ def solution(maps):
     d = l[2]
     queue = deque([[x,y,0]])
     visited = [[0 for _ in range(n)] for _ in range(m)]
-    e = bfs2(maps,dx,dy,visited,m,n,queue)
+    e = bfs(maps,dx,dy,visited,m,n,queue,"E")
     if e == -1:
         return -1
     
-    return d + e
+    return d + e[2]
 
-def bfs(maps,dx,dy,visited,m,n,queue):
+def bfs(maps,dx,dy,visited,m,n,queue,alpha):
     while queue:
         x,y,depth = queue.popleft()
-        if maps[x][y] == "L":
+        if maps[x][y] == alpha:
             return [x,y,depth]
         for i in range(4):
             nx = x + dx[i]
@@ -39,18 +39,6 @@ def bfs(maps,dx,dy,visited,m,n,queue):
                 queue.append([nx,ny,depth+1])
     return -1
 
-def bfs2(maps,dx,dy,visited,m,n,queue):
-    while queue:
-        x,y,depth = queue.popleft()
-        if maps[x][y] == "E":
-            return depth
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n and visited[nx][ny] == 0 and maps[nx][ny] != "X":
-                visited[nx][ny] = 1
-                queue.append([nx,ny,depth+1])
-    return -1
 
         
         
