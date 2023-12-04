b4484daec8a3b928d7cd8c963c2f06c9012b0776
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-12-04 21:46:07 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 6.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,17 +1,34 @@
 def solution(n, l, r):
-    answer = 0
 
-    for l in range(l-1, r):
-        if check(l):
-            answer += 1
+    if n == 0:
+        return min(r, 1) - max(l - 1, 0)
 
-    return answer
+    # Calculate the length of the string after n iterations
+    length = 1 * (5 ** n)
 
-def check(i):
-    if i % 5 == 2:
-        return False
-    if i < 5:
-        return True
+    # Function to count the number of 1's in a given range
+    def count_in_range(l, r, length, depth):
+        if depth == 0 or l > length or r < 1:
+            return 0
+        if l <= 1 and r >= length:
+            # Calculate the total number of 1's at this depth
+            return (4 ** depth)
 
+        mid = length // 2 + 1
+        total_count = 0
 
-    return check(i // 5)
+        # Count 1's in the left half (originally 1 -> 11011)
+        if l <= mid:
+            total_count += count_in_range(l, min(mid, r), length // 5, depth - 1)
+
+        # Count 1's in the right half (originally 1 -> 11011)
+        if r > mid:
+            total_count += count_in_range(max(1, l - mid), r - mid, length // 5, depth - 1)
+
+        # If the middle position is in range, count it (it's always 1)
+        if l <= mid <= r:
+            total_count += 1
+
+        return total_count
+
+    return count_in_range(l, r, length, n)
