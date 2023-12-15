54dd99253407337071284485b0b45e337fe10867
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-12-15 21:05:50 +0900

    [level 3] Title: 기지국 설치, Time: 5.30 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#) 
 
 ### 성능 요약
 
-메모리: 12 MB, 시간: 6.02 ms
+메모리: 11.8 MB, 시간: 5.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 21:3:11
+2023년 12월 5일 21:5:47
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -5,12 +5,8 @@ def solution(n, stations, w):
     for i in stations:
         arr.append([i-w,i+w])
     for i in arr:
-        if (i[0]-1-p)%(w*2+1) == 0:
-            answer -=1
-        answer += (i[0]-1-p)//(w*2+1) +1
+        answer += -(-(i[0]-1-p)//(w*2+1))
         p = i[1]
     if p < n:
-        if (n-p)%(w*2+1) == 0:
-            answer -= 1
-        answer += (n-p)//(w*2+1) + 1
+        answer += -(-(n-p)//(w*2+1))
     return answer
